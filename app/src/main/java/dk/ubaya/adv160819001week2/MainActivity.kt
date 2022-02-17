0047cddbfd329f2c6997cf2982735e6ea3695d87
David Kurniawan <s160819001@student.ubaya.ac.id>
2022-02-17 09:41:09 +0700

    init

---
+++ app/src/main/java/dk/ubaya/adv160819001week2/MainActivity.kt
@@ -0,0 +1,11 @@
+package dk.ubaya.adv160819001week2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
