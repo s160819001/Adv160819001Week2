be8e58ff835fc13bb18f3cd6d01999d69af46688
David Kurniawan <s160819001@student.ubaya.ac.id>
2022-02-24 10:24:46 +0700

    edited until 26.59 softbackbutton clear caused by hostfragment nav controller solved by https://stackoverflow.com/questions/58703451/fragmentcontainerview-as-navhostfragment

--- app/src/main/java/dk/ubaya/adv160819001week2/MainActivity.kt
+++ app/src/main/java/dk/ubaya/adv160819001week2/MainActivity.kt
@@ -1,24 +1,32 @@
 package dk.ubaya.adv160819001week2
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        navController = Navigation.findNavController(this, R.id.hostFragment)
-//        NavigationUI.setupActionBarWithNavController(this,navController)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this,navController)
+
+        bottomNav.setupWithNavController(navController)
     }
 
-//    override fun onSupportNavigateUp(): Boolean {
-//        return NavigationUI.navigateUp(navController,null)
-//    }
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController,null)
+    }
+
 }
